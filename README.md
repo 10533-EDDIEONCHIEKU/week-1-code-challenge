e3b3d310361d2194480f3c9e9697c698f321f6c8
10533-EDDIEONCHIEKU <onchiekualways@gmail.com>
2023-06-19 02:25:30 +0300

    completed assignment

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# week-1-code-challenge
-This is week two code challenge
+#week-1-code-challenge
+
+
+Student Grade Generator
+The Student Grade Generator is a JavaScript program that prompts the user to input a student's marks and determines the corresponding grade based on the provided grading scale. The program expects the input marks to be between 0 and 100 and assigns grades according to specific ranges. Here's how it works:
+
+The program asks the user to input the student's marks.
+It then checks the input marks against the grading scale to determine the appropriate grade.
+If the marks are greater than 79, it assigns grade "A".
+If the marks are between 60 and 79 (inclusive), it assigns grade "B".
+If the marks are between 50 and 59 (inclusive), it assigns grade "C".
+If the marks are between 40 and 49 (inclusive), it assigns grade "D".
+If the marks are less than 40, it assigns grade "E".
+Finally, the program displays the calculated grade.
+
+
+Speed Detector
+The Speed Detector is a JavaScript program that takes as input the speed of a car and determines if it is within the speed limit. It provides feedback to the driver and calculates demerit points based on the speed. Here's how it works:
+
+The program prompts the user to enter the speed of the car in kilometers per hour.
+It checks if the speed is less than 70 km/h. If it is, the program prints "Ok" as the speed is within the limit.
+If the speed exceeds 70 km/h, the program calculates the number of demerit points. For every 5 km/h above the speed limit, one demerit point is added.
+The program displays the total number of demerit points.
+If the number of demerit points exceeds 12, the program prints "License suspended" to indicate that the driver's license is suspended.
+
+
+
+Net Salary Calculator
+The Net Salary Calculator is a JavaScript program that calculates an individual's net salary based on their basic salary and benefits. It also determines the payee (tax), NHIF deductions, NSSF deductions, gross salary, and net salary. Here's how it works:
+
+The program prompts the user to enter the individual's basic salary and benefits.
+It calculates the gross salary by adding the basic salary and benefits together.
+The program then determines the payee (tax) by multiplying the gross salary by the tax rate (0.2).
+Next, it calculates the NHIF deductions by taking the lesser of the gross salary multiplied by the NHIF rate (0.02) or the NHIF maximum amount (5000).
+Similarly, it calculates the NSSF deductions by taking the lesser of the gross salary multiplied by the NSSF rate (0.1) or the NSSF maximum amount (2000).
+Finally, the program calculates the net salary by subtracting the payee, NHIF deductions, and NSSF deductions from the gross salary.
--- script.js
+++ script.js
@@ -1,71 +1,72 @@
-let grade=document.getElementById('grade')
-let speed=document.getElementById('speed')
-let salary=document.getElementById('salary')
+//CHALLENGE ONE
+let mark = parseInt(prompt("Enter the student's mark (between 0 and 100):"));
 
+let grade;
 
-grade.addEventListener('click', (e)=>{
-e.preventDefault()
-let inputGrade=parseInt(document.getElementById('input-grade').value)
-console.log(inputGrade)
-let finalOutput
-// write Code For the Code challenge
-n = float(input('Enter the percentage of the student: '))
-
-if(n > 79);
-print("Grade A")
-if(n > 60);
-print("Grade B")
-if(n > 49);
-print("Grade C")
-if(n >= 40);
-print("Grade D")
-else:
-print("Grade E")
-
-finalOutput='assign your final output to this variable'
-document.getElementById('marks-id').append(finalOutput)
-
-})
-speed.addEventListener('click', (e)=>{
-    e.preventDefault()
-    let inputSpeed=parseInt(document.getElementById('input-speed').value)
-    console.log(inputSpeed)
-    let finalOutput
-// write Code For the Code challenge
-
-if(speed<70)print('Ok')
-else
-{
-print("Points:"+((speed-70)/5));
+if (mark > 79) {
+  grade = 'A';
+} else if (mark >= 60) {
+  grade = 'B';
+} else if (mark >= 50) {
+  grade = 'C';
+} else if (mark >= 40) {
+  grade = 'D';
+} else {
+  grade = 'E';
 }
 
-if((speed-70)/5)>12;){
-print('License suspended');
+console.log("The student's grade is: " + grade);
+
+
+//CHALLNGE TWO SPEED
+let speed = parseInt(prompt("Enter the car's speed (in km/h):"));
+const speedLimit = 70;
+let demeritPoints = 0;
+
+if (speed < speedLimit) {
+  console.log("Ok");
+} else {
+  demeritPoints = Math.floor((speed - speedLimit) / 5);
+  
+  if (demeritPoints > 12) {
+    console.log("License suspended");
+  } else {
+    console.log("Points:", demeritPoints);
+  }
 }
-finalOutput='assign your final output to this variable'
-    document.getElementById('speed-id').append(finalOutput)
 
-})
+// challenge three
 
+// Input values
 
-salary.addEventListener('click', (e)=>{
-        e.preventDefault()
-        let inputSalary=parseInt(document.getElementById('input-salary').value)
-        console.log(inputSalary)
-let finalOutput
-// write Code For the Code challenge
+let basicSalary = parseFloat(prompt("Enter the basic salary:"));
+let benefits = parseFloat(prompt("Enter the benefits:"));
 
+// Constants for tax rates and deductions
+const taxRate = 0.2;
+const nhifRate = 0.02;
+const nssfRate = 0.1;
+const nhifMax = 5000;
+const nssfMax = 2000;
 
+// Calculate gross salary
+let grossSalary = basicSalary + benefits;
 
+// Calculate payee (tax)
+let payee = grossSalary * taxRate;
 
+// Calculate NHIF deductions
+let nhifDeductions = Math.min(grossSalary * nhifRate, nhifMax);
 
+// Calculate NSSF deductions
+let nssfDeductions = Math.min(grossSalary * nssfRate, nssfMax);
 
+// Calculate net salary
+let netSalary = grossSalary - payee - nhifDeductions - nssfDeductions;
 
-
-
-
-
-finalOutput ='assign your final output to this variable'
-document.getElementById('salary-id').append(finalOutput)
-        
-})
+// Print the results
+console.log("Gross Salary:", grossSalary);
+console.log("Payee (Tax):", payee);
+console.log("NHIF Deductions:", nhifDeductions);
+console.log("NSSF Deductions:", nssfDeductions);
+console.log("Net Salary:", netSalary);
